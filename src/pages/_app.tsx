5fff54f16f1112d3d664aea52085e42979b4158d
Sam Margalit <sam@margalit.com.au>
2023-01-21 16:34:40 +1100

    Init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { Reshaped } from "reshaped";
+import "reshaped/themes/reshaped/theme.css";
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { api } from "../utils/api";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Reshaped theme="reshaped">
+        <Component {...pageProps} />
+      </Reshaped>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
