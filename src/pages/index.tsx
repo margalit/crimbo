737f6d1e630d114c780a228719005987ef179946
Sam Margalit <sam@margalit.com.au>
2023-01-29 11:55:50 +1100

    More players

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import { type NextPage } from "next";
-import type { Session } from "next-auth";
 import {
   Container,
   Button,
   Avatar,
   View,
-  Actionable,
-  DropdownMenu,
   Text,
   Image,
   Card,
@@ -15,18 +12,18 @@ import {
   ActionBar,
   useTheme,
   Overlay,
+  Link,
 } from "reshaped";
 import {
   TrophyIcon,
-  PhotoIcon,
+  CameraIcon,
   MoonIcon,
   SunIcon,
   ChevronDownIcon,
+  InformationCircleIcon,
 } from "@heroicons/react/20/solid";
+import { MapPinIcon } from "@heroicons/react/24/solid";
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
-
-import { api } from "../utils/api";
 
 type Entries<T> = {
   [K in keyof T]: [K, T[K]];
@@ -35,73 +32,125 @@ type Entries<T> = {
 const getEntries = <T extends object>(obj: T) =>
   Object.entries(obj) as Entries<T>;
 
-const participants = {
-  sam: {
-    name: "Sam",
-    image: "/sam.jpeg",
+const players = {
+  samM: {
+    name: "Sam M",
+    image: "/players/samM.jpeg",
   },
-  ruby: {
-    name: "Ruby",
-    image: "/ruby.jpeg",
+  rubyB: {
+    name: "Ruby B",
+    image: "/players/rubyB.jpeg",
   },
-  jo: {
-    name: "Jo",
-    image: "/jo.jpeg",
+  joL: {
+    name: "Jo L",
+    image: "/players/joL.jpeg",
   },
-  chris: {
-    name: "Chris",
-    image: "/chris.jpeg",
+  chrisV: {
+    name: "Chris V",
+    image: "/players/chrisV.jpeg",
+  },
+  maxW: {
+    name: "Max W",
+    image: "/players/maxW.png",
+  },
+  jessH: {
+    name: "Jess H",
+    image: "/players/jessH.png",
+  },
+  chrisM: {
+    name: "Chris M",
+    image: "/players/chrisM.png",
   },
 };
 
 const feed = [
   {
+    id: "jan23-3",
+    src: "/harvests/jan23-3.jpeg",
+    claimant: "chrisM" as const,
+    location: "Flinders St, Thornbury",
+    points: 1,
+    date: "Jan 23",
+  },
+  {
+    id: "jan23-2",
+    src: "/harvests/jan23-2.jpeg",
+    claimant: "jessH" as const,
+    location: "Near the train station, Thornbury",
+    points: 1,
+    date: "Jan 23",
+  },
+  {
+    id: "jan23",
+    src: "/harvests/jan23.jpeg",
+    claimant: "maxW" as const,
+    location: "Rennie St, Thornbury",
+    points: 1,
+    date: "Jan 23",
+  },
+  {
+    id: "jan22",
+    src: "/harvests/jan22.jpeg",
+    claimant: "rubyB" as const,
+    location: "Darebin Street, Heidelberg",
+    points: 1,
+    date: "Jan 22",
+  },
+  {
     id: "1",
-    src: "/one.jpeg",
-    claimant: "sam" as const,
-    street: "Franklin Street",
+    src: "/harvests/one.jpeg",
+    claimant: "samM" as const,
+    location: "Franklin Street, Coburg",
     points: 1,
     date: "Jan 18",
   },
   {
     id: "2",
-    src: "/two.jpeg",
-    claimant: "ruby" as const,
-    street: "Unknown",
+    src: "/harvests/two.jpeg",
+    claimant: "rubyB" as const,
+    location: "Coburg",
     points: 1,
     date: "Jan 16",
   },
   {
     id: "3",
-    src: "/three.jpeg",
-    claimant: "ruby" as const,
-    street: "De Carle Street",
+    src: "/harvests/three.jpeg",
+    claimant: "rubyB" as const,
+    location: "De Carle Street, Coburg",
     points: 1,
     date: "Jan 16",
   },
   {
     id: "4",
-    src: "/four.jpeg",
-    claimant: "ruby" as const,
-    street: "Unknown",
+    src: "/harvests/four.jpeg",
+    claimant: "rubyB" as const,
+    location: "Coburg North",
     points: 1,
     date: "Jan 3",
   },
   {
     id: "5",
-    src: "/five.jpeg",
-    claimant: "ruby" as const,
-    street: "Unknown",
+    src: "/harvests/five.jpeg",
+    claimant: "rubyB" as const,
+    location: "Clayton",
     points: 1,
     date: "Jan 3",
   },
   {
     id: "6",
-    src: "/six.jpeg",
-    claimant: "ruby" as const,
-    street: "Unknown",
+    src: "/harvests/six.jpeg",
+    claimant: "rubyB" as const,
+    location: "Outside Vinnies, Preston",
     points: 0.5,
     date: "Jan 3",
+  },
+  {
+    id: "7",
+    src: "/harvests/seven.jpg",
+    claimant: "chrisV" as const,
+    location: "Tumbarumba",
+    points: 5,
+    date: "Dec 27",
   },
 ];
 
@@ -112,10 +161,13 @@ const totals = feed.reduce(
     return totals;
   },
   {
-    sam: { points: 0, harvests: 0 },
-    ruby: { points: 0, harvests: 0 },
-    jo: { points: 0, harvests: 0 },
-    chris: { points: 0, harvests: 0 },
+    samM: { points: 0, harvests: 0 },
+    rubyB: { points: 0, harvests: 0 },
+    joL: { points: 0, harvests: 0 },
+    chrisV: { points: 0, harvests: 0 },
+    maxW: { points: 0, harvests: 0 },
+    jessH: { points: 0, harvests: 0 },
+    chrisM: { points: 0, harvests: 0 },
   }
 );
 
@@ -131,45 +183,70 @@ function pointsText(points: number) {
 }
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
         <title>crimbo</title>
-        <meta name="description" content="Harvest your neighbourhood" />
+        <meta
+          name="description"
+          content="Find and share old Christmas trees to win"
+        />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="og:title"
+          content="Crimbo: Find and share old Christmas trees"
+        />
+        <meta
+          name="og:description"
+          content="Crimbo is a game where each player tries to take photos of discarded Christmas trees."
+        />
+        <meta
+          name="og:image"
+          content={`${
+            process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
+          }/hero.jpeg`}
+        />
       </Head>
       <Container width="580px">
-        <View gap={4} paddingTop={4} paddingBottom={4}>
+        <View gap={4} paddingTop={4} paddingBottom={12}>
           <View align="center" direction="row">
             <View.Item grow>
-              <Text variant="body-strong-1">crimbo</Text>
+              <Text variant="body-strong-1">Crimbo</Text>
             </View.Item>
             <Navigation />
           </View>
           <Tabs itemWidth="equal" variant="pills-elevated">
             <Tabs.List>
-              <Tabs.Item value="0" icon={<PhotoIcon />}>
-                <Text variant="body-medium-2">Feed</Text>
+              <Tabs.Item value="0" icon={<CameraIcon />}>
+                <Text variant="body-medium-2">Harvests</Text>
               </Tabs.Item>
               <Tabs.Item value="1" icon={<TrophyIcon />}>
-                <Text variant="body-medium-2">Scores</Text>
+                <Text variant="body-medium-2">Leaderboard</Text>
+              </Tabs.Item>
+              <Tabs.Item value="2" icon={<InformationCircleIcon />}>
+                <Text variant="body-medium-2">About</Text>
               </Tabs.Item>
             </Tabs.List>
             <Tabs.Panel value="0">
-              <View gap={6}>
-                {feed.map(({ id, src, claimant, street, date, points }) => (
+              <View gap={6} paddingTop={4}>
+                {feed.map(({ id, src, claimant, location, date, points }) => (
                   <Card key={id} padding={0}>
                     <Overlay
                       position="bottom"
                       backgroundSlot={
                         <AspectRatio ratio={1}>
-                          <Image src={src} alt="discarded christmas tree" />
+                          <Image
+                            src={src}
+                            alt="discarded christmas tree"
+                            width="100%"
+                          />
                         </AspectRatio>
                       }
                     >
-                      <Text variant="title-1">{street}</Text>
+                      <View direction="row" align="center" gap={1}>
+                        <MapPinIcon width={24} height={24} />
+                        <Text variant="title-3">{location}</Text>
+                      </View>
                     </Overlay>
                     <ActionBar>
                       <View
@@ -178,15 +255,14 @@ const Home: NextPage = () => {
                         direction="row"
                         position="relative"
                       >
-                        <Avatar src={participants[claimant].image} size={10} />
+                        <Avatar src={players[claimant].image} size={10} />
                         <View.Item grow>
                           <View align="center" direction="row" gap={1}>
                             <Text variant="body-medium-1">
-                              {participants[claimant].name} harvested{" "}
-                              {pointsText(points)}
+                              {players[claimant].name} got {pointsText(points)}
                             </Text>
                           </View>
-                          <Text color="neutral-faded">Claimed on {date}</Text>
+                          <Text color="neutral-faded">Harvested on {date}</Text>
                         </View.Item>
                       </View>
                     </ActionBar>
@@ -195,16 +271,16 @@ const Home: NextPage = () => {
               </View>
             </Tabs.Panel>
             <Tabs.Panel value="1">
-              <View gap={4}>
+              <View gap={4} paddingTop={4}>
                 {getEntries(totals)
                   .sort(([, a], [, b]) => (a.points > b.points ? -1 : 1))
-                  .map(([id, totals], index) => (
+                  .map(([id, totals]) => (
                     <Card key={id}>
                       <View direction="row" align="center" gap={4}>
-                        <Avatar src={participants[id].image} size={12} />
+                        <Avatar src={players[id].image} size={12} />
                         <View>
                           <Text variant="body-strong-1">
-                            {participants[id].name}
+                            {players[id].name}
                           </Text>
                           <Text variant="body-1" color="neutral-faded">
                             {totals.points === 1
@@ -221,6 +297,55 @@ const Home: NextPage = () => {
                   ))}
               </View>
             </Tabs.Panel>
+            <Tabs.Panel value="2">
+              <View gap={6} paddingTop={4}>
+                <AspectRatio ratio={1.6}>
+                  <Image
+                    src="/hero.jpeg"
+                    borderRadius="large"
+                    alt="Discarded christmas tree"
+                  />
+                </AspectRatio>
+                <Text variant="title-3">
+                  Crimbo is a game where each player tries to take photos of
+                  discarded Christmas trees.
+                </Text>
+                <View gap={1}>
+                  <Text variant="title-3">Harvests</Text>
+                  <Text variant="body-1">
+                    Photos of trees are called ‘harvests’. Players receive more
+                    points later in the year as trees get rarer making it
+                    possible to win with just a few late harvests.
+                  </Text>
+                </View>
+                <View gap={1}>
+                  <Text variant="title-3">Rules</Text>
+                  <Text variant="body-1">
+                    <li>Harvests must be unique</li>
+                    <li>Trees must be on the way out, not the way in</li>
+                    <li>The leaderboard resets on Christmas Day</li>
+                  </Text>
+                </View>
+                <View gap={1}>
+                  <Text variant="title-3">Points</Text>
+                  <Text variant="body-1">
+                    <li>+1 point for each month past December</li>
+                    <li>1/2 points for synthetic trees</li>
+                    <li>
+                      5 points for harvests between Boxing Day and New Years
+                    </li>
+                  </Text>
+                </View>
+                <View gap={1}>
+                  <Text variant="title-3">Can I play?</Text>
+                  <Text variant="body-1">
+                    Sure! The feed is currently manual so please send harvests
+                    via <Link href="mailto:sam@margalit.com.au">email</Link>.
+                    Please include a location so I can verify it’s uniqueness!
+                  </Text>
+                </View>
+              </View>
+            </Tabs.Panel>
           </Tabs>
         </View>
       </Container>
@@ -231,7 +356,6 @@ const Home: NextPage = () => {
 export default Home;
 
 const Navigation = () => {
-  const { data: sessionData } = useSession();
   return (
     <View justify="end" align="center" direction="row" gap={3}>
       <ColorModeButton />
@@ -267,27 +391,27 @@ const ColorModeButton = () => {
   );
 };
 
-interface UserProps {
-  user: Session["user"];
-}
+// interface UserProps {
+//   user: Session["user"];
+// }
 
-const User = ({ user }: UserProps) => (
-  <DropdownMenu position="bottom">
-    <DropdownMenu.Trigger>
-      {(attributes) => (
-        <Actionable attributes={attributes}>
-          {user?.image ? (
-            <Avatar size={8} src={user.image} />
-          ) : (
-            <Text>Signed in</Text>
-          )}
-        </Actionable>
-      )}
-    </DropdownMenu.Trigger>
-    <DropdownMenu.Content>
-      <DropdownMenu.Item onClick={() => void signOut()}>
-        Sign out
-      </DropdownMenu.Item>
-    </DropdownMenu.Content>
-  </DropdownMenu>
-);
+// const User = ({ user }: UserProps) => (
+//   <DropdownMenu position="bottom">
+//     <DropdownMenu.Trigger>
+//       {(attributes) => (
+//         <Actionable attributes={attributes}>
+//           {user?.image ? (
+//             <Avatar size={8} src={user.image} />
+//           ) : (
+//             <Text>Signed in</Text>
+//           )}
+//         </Actionable>
+//       )}
+//     </DropdownMenu.Trigger>
+//     <DropdownMenu.Content>
+//       <DropdownMenu.Item onClick={() => void signOut()}>
+//         Sign out
+//       </DropdownMenu.Item>
+//     </DropdownMenu.Content>
+//   </DropdownMenu>
+// );
