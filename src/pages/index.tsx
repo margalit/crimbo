262ea5b14121b5b46f8c855f4b81e7cf11b73ffe
Sam Margalit <sam@margalit.com.au>
2023-01-29 14:15:41 +1100

    Hero priority

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import {
   Avatar,
   View,
   Text,
-  Image,
   Card,
   AspectRatio,
   Tabs,
@@ -13,7 +12,6 @@ import {
   useTheme,
   Overlay,
   Link,
-  Placeholder,
 } from "reshaped";
 import {
   TrophyIcon,
@@ -281,17 +279,67 @@ const Home: NextPage = () => {
           </View>
           <Tabs itemWidth="equal" variant="pills-elevated">
             <Tabs.List>
-              <Tabs.Item value="0" icon={<CameraIcon />}>
+              <Tabs.Item value="0" icon={<InformationCircleIcon />}>
+                <Text variant="body-medium-2">About</Text>
+              </Tabs.Item>
+              <Tabs.Item value="1" icon={<CameraIcon />}>
                 <Text variant="body-medium-2">Harvests</Text>
               </Tabs.Item>
-              <Tabs.Item value="1" icon={<TrophyIcon />}>
+              <Tabs.Item value="2" icon={<TrophyIcon />}>
                 <Text variant="body-medium-2">Leaderboard</Text>
-              </Tabs.Item>
-              <Tabs.Item value="2" icon={<InformationCircleIcon />}>
-                <Text variant="body-medium-2">About</Text>
               </Tabs.Item>
             </Tabs.List>
             <Tabs.Panel value="0">
+              <View gap={6} paddingTop={4}>
+                <View borderRadius="large" overflow="hidden">
+                  <NextImage
+                    src={HeroImage}
+                    width={548}
+                    priority
+                    alt="Discarded christmas tree"
+                  />
+                </View>
+                <Text variant="title-3">
+                  Crimbo is a game where each player tries to take photos of
+                  discarded Christmas trees.
+                </Text>
+                <View gap={1}>
+                  <Text variant="title-3">Harvests</Text>
+                  <Text variant="body-1">
+                    Photos of trees are called ‘harvests’. Players receive more
+                    points later in the year as trees get rarer making it
+                    possible to win with just a few late harvests.
+                  </Text>
+                </View>
+                <View gap={1}>
+                  <Text variant="title-3">Rules</Text>
+                  <Text variant="body-1">
+                    <li>Harvests must be unique</li>
+                    <li>Trees must be on the way out, not the way in</li>
+                    <li>The leaderboard resets on Christmas Day</li>
+                  </Text>
+                </View>
+                <View gap={1}>
+                  <Text variant="title-3">Points</Text>
+                  <Text variant="body-1">
+                    <li>+1 point for each month past December</li>
+                    <li>1/2 points for synthetic trees</li>
+                    <li>
+                      5 points for harvests between Boxing Day and New Years
+                    </li>
+                  </Text>
+                </View>
+                <View gap={1}>
+                  <Text variant="title-3">Can I play?</Text>
+                  <Text variant="body-1">
+                    Sure! The feed is currently manual so please send harvests
+                    via <Link href="mailto:sam@margalit.com.au">email</Link>.
+                    Please include a location so I can verify it’s uniqueness!
+                  </Text>
+                </View>
+              </View>
+            </Tabs.Panel>
+            <Tabs.Panel value="1">
               <View gap={6} paddingTop={4}>
                 {feed.map(
                   ({ id, src, claimant, location, date, points }, index) => (
@@ -341,7 +389,7 @@ const Home: NextPage = () => {
                 )}
               </View>
             </Tabs.Panel>
-            <Tabs.Panel value="1">
+            <Tabs.Panel value="2">
               <View gap={4} paddingTop={4}>
                 {getEntries(totals)
                   .sort(([, a], [, b]) => (a.points > b.points ? -1 : 1))
@@ -366,55 +414,6 @@ const Home: NextPage = () => {
                       </View>
                     </Card>
                   ))}
-              </View>
-            </Tabs.Panel>
-            <Tabs.Panel value="2">
-              <View gap={6} paddingTop={4}>
-                <View borderRadius="large" overflow="hidden">
-                  <NextImage
-                    src={HeroImage}
-                    width={548}
-                    alt="Discarded christmas tree"
-                  />
-                </View>
-                <Text variant="title-3">
-                  Crimbo is a game where each player tries to take photos of
-                  discarded Christmas trees.
-                </Text>
-                <View gap={1}>
-                  <Text variant="title-3">Harvests</Text>
-                  <Text variant="body-1">
-                    Photos of trees are called ‘harvests’. Players receive more
-                    points later in the year as trees get rarer making it
-                    possible to win with just a few late harvests.
-                  </Text>
-                </View>
-                <View gap={1}>
-                  <Text variant="title-3">Rules</Text>
-                  <Text variant="body-1">
-                    <li>Harvests must be unique</li>
-                    <li>Trees must be on the way out, not the way in</li>
-                    <li>The leaderboard resets on Christmas Day</li>
-                  </Text>
-                </View>
-                <View gap={1}>
-                  <Text variant="title-3">Points</Text>
-                  <Text variant="body-1">
-                    <li>+1 point for each month past December</li>
-                    <li>1/2 points for synthetic trees</li>
-                    <li>
-                      5 points for harvests between Boxing Day and New Years
-                    </li>
-                  </Text>
-                </View>
-                <View gap={1}>
-                  <Text variant="title-3">Can I play?</Text>
-                  <Text variant="body-1">
-                    Sure! The feed is currently manual so please send harvests
-                    via <Link href="mailto:sam@margalit.com.au">email</Link>.
-                    Please include a location so I can verify it’s uniqueness!
-                  </Text>
-                </View>
               </View>
             </Tabs.Panel>
           </Tabs>
