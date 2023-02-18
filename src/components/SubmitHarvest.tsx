9cb39a14011ba77b45c8a8d8b9f5827d337c5955
Sam Margalit <sam@margalit.com.au>
2023-02-19 08:05:30 +1100

    Basic error handling

--- src/components/SubmitHarvest.tsx
+++ src/components/SubmitHarvest.tsx
@@ -10,52 +10,93 @@ import {
   RadioGroup,
   Radio,
   useToast,
+  Alert,
 } from "reshaped";
 import { supabase } from "../lib/supabaseClient";
 import { uuid } from "uuidv4";
 import { useData } from "../pages";
-import { useState } from "react";
+import useSWRMutation from "swr/mutation";
+import type { Database } from "../types/supabase";
 
 interface SubmitHarvestProps {
   user: User;
 }
 
+interface UploadImageArgs {
+  arg: File;
+}
+
+async function uploadImage(key: string, { arg: image }: UploadImageArgs) {
+  const { data, error } = await supabase.storage
+    .from("harvests")
+    .upload(uuid(), image);
+
+  if (error) {
+    throw new Error("Image upload issue");
+  }
+
+  return data;
+}
+
+async function insertHarvest(
+  key: string,
+  { arg }: { arg: Database["public"]["Tables"]["harvests"]["Insert"] }
+) {
+  const { data, error } = await supabase.from("harvests").insert(arg);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
 const SubmitHarvest = ({ user }: SubmitHarvestProps) => {
-  const [isLoading, setIsLoading] = useState(false);
   const { active, activate, deactivate } = useToggle(false);
-  const { mutate } = useData();
+  const { mutate: refetch } = useData();
+  const upload = useSWRMutation("upload", uploadImage);
+  const insert = useSWRMutation("insert", insertHarvest);
   const toast = useToast();
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setIsLoading(true);
     const formData = new FormData(event.currentTarget);
     const image = formData.get("image") as File;
 
-    const upload = await supabase.storage
-      .from("harvests")
-      .upload(uuid(), image);
+    try {
+      const imageData = await upload.trigger(image);
+      if (imageData) {
+        await insert.trigger({
+          claimant: 1,
+          user: user.id,
+          location: formData.get("location") as string,
+          image: imageData.path,
+          is_synthetic: formData.get("is_synthetic") === "true",
+        });
 
-    if (upload.data) {
-      await supabase.from("harvests").insert({
-        claimant: 1,
-        user: user.id,
-        location: formData.get("location") as string,
-        image: upload.data.path,
-        is_synthetic: formData.get("is_synthetic") === "true",
-      });
-
-      setIsLoading(false);
-
+        await refetch();
+        toast.show({
+          text: "Harvest submitted!",
+          position: "top",
+        });
+        deactivate();
+      }
+    } catch (err) {
       toast.show({
-        text: "Harvest submitted!",
+        text: "There was an issue submitting your harvest",
+        position: "top",
       });
-
-      await mutate();
-
-      deactivate();
     }
   };
+
+  let error: boolean | Error = false;
+  if (upload.error instanceof Error) {
+    error = upload.error;
+  }
+  if (insert.error instanceof Error) {
+    error = insert.error;
+  }
+
   return (
     <>
       <Button size="small" variant="outline" onClick={activate}>
@@ -70,6 +111,12 @@ const SubmitHarvest = ({ user }: SubmitHarvestProps) => {
           <Dismissible onClose={deactivate} closeAriaLabel="Close modal">
             <Modal.Title>Submit harvest</Modal.Title>
           </Dismissible>
+          {error && (
+            <Alert title="Something went wrong" color="critical">
+              We couldn’t upload your harvest, here’s what went wrong:{" "}
+              <strong>{error.message}</strong>
+            </Alert>
+          )}
           <form onSubmit={(event) => void onSubmit(event)}>
             <View gap={3}>
               <FormControl required>
@@ -91,7 +138,11 @@ const SubmitHarvest = ({ user }: SubmitHarvestProps) => {
                   </View>
                 </RadioGroup>
               </FormControl>
-              <Button loading={isLoading} disabled={isLoading} type="submit">
+              <Button
+                loading={upload.isMutating}
+                disabled={upload.isMutating}
+                type="submit"
+              >
                 Submit
               </Button>
             </View>
